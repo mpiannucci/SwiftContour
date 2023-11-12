7abb1892974f3f741d5663ec6fe44ba3fe87c672
Matthew Iannucci <mpiannucci@gmail.com>
2023-11-11 21:17:05 -0500

    Add test cases with current failures

--- Tests/SwiftContourTests/SwiftContourTests.swift
+++ Tests/SwiftContourTests/SwiftContourTests.swift
@@ -62,4 +62,115 @@ final class SwiftContourTests: XCTestCase {
         let coords = polygons.coordinates[0].coordinates[0].coordinates
         XCTAssert(coords.count == 17)
     }
+    
+    func testPolygonWithHole() throws {
+        let builder = ContourBuilder(dx: 10, dy: 10, smooth: true, x_origin: 0.0, y_origin: 0.0, x_step: 1.0, y_step: 1.0)
+        let data = [
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ]
+        
+        let contours = try builder.contours(values: data, thresholds: [0.5])
+        XCTAssert(contours.count == 1)
+        guard case let .multiPolygon(polygons) = contours[0].geometry else {
+            return
+        }
+        
+        let rings = polygons.coordinates[0].coordinates
+        XCTAssert(rings.count == 2)
+    }
+    
+    func testMultiPolygon() throws {
+        let builder = ContourBuilder(dx: 10, dy: 10, smooth: true, x_origin: 0.0, y_origin: 0.0, x_step: 1.0, y_step: 1.0)
+        let data = [
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ]
+        
+        let contours = try builder.contours(values: data, thresholds: [0.5])
+        XCTAssert(contours.count == 1)
+        guard case let .multiPolygon(polygons) = contours[0].geometry else {
+            return
+        }
+        
+        XCTAssert(polygons.coordinates.count == 2)
+    }
+    
+    func testMultiPolygonWithHole() throws {
+        let builder = ContourBuilder(dx: 10, dy: 10, smooth: true, x_origin: 0.0, y_origin: 0.0, x_step: 1.0, y_step: 1.0)
+        let data = [
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0,
+            0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ]
+        
+        let contours = try builder.contours(values: data, thresholds: [0.5])
+        XCTAssert(contours.count == 1)
+        guard case let .multiPolygon(polygons) = contours[0].geometry else {
+            return
+        }
+        
+        XCTAssert(polygons.coordinates.count == 2)
+    }
+    
+    func testMultipleThresholds() throws {
+        let builder = ContourBuilder(dx: 10, dy: 10, smooth: true, x_origin: 0.0, y_origin: 0.0, x_step: 1.0, y_step: 1.0)
+        let data = [
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 2.0, 2.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 2.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ]
+        
+        let contours = try builder.contours(values: data, thresholds: [0.5, 1.5])
+        XCTAssert(contours.count == 2)
+    }
+    
+    func testMultiPolygonWithXYSteps() throws {
+        let builder = ContourBuilder(dx: 10, dy: 10, smooth: true, x_origin: 100.0, y_origin: 200.0, x_step: 2.0, y_step: 2.0)
+        let data = [
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ]
+        
+        let contours = try builder.contours(values: data, thresholds: [0.5])
+        XCTAssert(contours.count == 1)
+    }
 }
